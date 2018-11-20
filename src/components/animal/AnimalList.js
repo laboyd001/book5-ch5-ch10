9e434a47b7e908050c5bbf748f01c0b395e7b2ae
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-19 20:14:29 -0600

    added form and new button button route isnt working

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -7,6 +7,17 @@ import { Link } from "react-router-dom";
 export default class AnimalList extends Component {
   render() {
     return (
+     <React.Fragment> 
+      <div className="animalButton">
+          <button type="button"
+                  className="btn btn-success"
+                  onClick={() => {
+                      this.props.history.push("/animals/new")}
+                  }>
+              Admit Animal
+          </button>
+      </div>
+      
       <section className="animals list">
         {this.props.animals.map(animal =>
           <div key={animal.id} className="card">
@@ -24,7 +35,9 @@ export default class AnimalList extends Component {
          )
         }
       </section>
+      </React.Fragment>
     )
   }
 }
+
 
