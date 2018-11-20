0a75f61b49644b07a5ab0f78cc8df816d70e3e2f
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-20 15:31:42 -0600

    added forms to all other components

---
+++ src/components/location/LocationForm.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./Location.css";
+
+ export default class LocationForm extends Component {
+  // Set initial state
+  state = {
+    locationName: "",
+    address: ""
+  };
+
+  // Update state whenever an input field is edited
+  handleFieldChange = evt => {
+    const stateToChange = {};
+    stateToChange[evt.target.id] = evt.target.value;
+    this.setState(stateToChange);
+  };
+
+  /*
+        Local method for validation, creating animal object, and
+        invoking the function reference passed from parent component
+     */
+  constructNewLocation = evt => {
+    evt.preventDefault();
+    if (this.state.address === "") {
+      window.alert("Please enter an address");
+    } else {
+      const kennel = {
+        name: this.state.locationName,
+        address: this.state.address,
+        };
+
+      // Create the animal and redirect user to animal list
+      this.props
+        .addLocation(kennel)
+        .then(() => this.props.history.push("/"));
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="locationForm newForm">
+          <div className="form-group">
+            <label htmlFor="locationName">Location name</label>
+            <input
+              type="text"
+              required="true"
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="locationName"
+              placeholder="Location name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              required="true"
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="address"
+              placeholder="Address"
+            />
+          </div>
+         
+          <button
+            type="submit"
+            onClick={this.constructNewLocation}
+            className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+
+
+
+
+ 
--- src/components/owner/OwnerList.js
+++ src/components/owner/OwnerList.js
@@ -7,6 +7,17 @@ import { Link } from "react-router-dom";
 export default class OwnerList extends Component {
   render() {
     return (
+      <React.Fragment>
+      <div className="ownerButton">
+          <button type="button"
+                  className="btn btn-success"
+                  onClick={() => {
+                      this.props.history.push("/owners/new")}
+                  }>
+              Add Owner
+          </button>
+      </div>
+
       <section className="owners list">
         {this.props.owners.map(owner =>
           <div key={owner.id} className="card">
@@ -24,6 +35,7 @@ export default class OwnerList extends Component {
          )
         }
       </section>
+      </React.Fragment>
     )
   }
 }
